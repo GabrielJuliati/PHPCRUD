a854c233fe9e83f34212f5cf0f106ab9377c3993
Allyson Kenzo Kawagoe <81180394+allysonkenzokawagoe@users.noreply.github.com>
2025-04-21 15:41:24 -0300

    Mudanças para deixar igual á main e alterações
    Foi alterado as pastas do projeto para ficar igual está na main. No código foi alterado o botão do agendamento e as options do mesmo para ficar com os exames a serem feitos.

--- front/agendamento/agendamento.php
+++ front/agendamento/agendamento.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../CSS/styleCP.css">
     <title>Document</title>
 </head>
 <body>
@@ -19,25 +20,24 @@
                 <h1 class="text-center m-5">Agendamento</h1>
                 <div>
                     <form action="">
-                        <div class="mb-3 text-center">
+                        <div class="mt-3 text-center">
                             <label for="name" class="form-label">Nome</label>
-                            <input type="text" name="name" id="name" class="form-control">
+                            <input type="text" name="name" id="name" class="form-control" required>
                         </div>
-                        <div class="m-3 text-center">
+                        <div class="mt-3 text-center">
                             <label for="date" class="form-label">Data do Exame:</label>
-                            <input type="date" name="date" id="date" class="form-control">
+                            <input type="date" name="date" id="date" class="form-control" required>
                         </div>
-                        <div class="mb-3 text-center">
+                        <div class="mt-3 text-center">
                             <label for="type" class="form-label">Tipo do Exame:</label>
-                            <select id="type" name="type" class="form-select">
-                                <option value="exame1">ex1</option>
-                                <option value="exame2">ex2</option>
-                                <option value="exame3">ex3</option>
-                                <option value="exame4">ex4</option>
+                            <select id="type" name="type" class="form-select" required>
+                                <option value="exame1">ABO - Tipo Sanguíneo</option>
+                                <option value="exame2">Dengue</option>
+                                <option value="exame3">COVID 19</option>
                             </select>
                         </div>
-                        <div class="mb-3 text-center">
-                            <input type="submit" value="Enviar" class="btn btn-success">
+                        <div class="mt-3 text-center">
+                            <input type="submit" value="Enviar" class="btn btn-primary">
                         </div>
                     </form>
                 </div>
@@ -45,6 +45,9 @@
             <div class="col"></div>
         </div>
     </div>
+    <?php
+        include('../modelo/footer.php');
+    ?>
 
 </body>
 </html>
